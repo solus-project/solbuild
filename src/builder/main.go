e7e150ca4903090abeb9f6a0310d7726c1cfc0b4
Ikey Doherty <ikey@solus-project.com>
2016-11-30 03:07:51 +0000

    builder: Create BackingImage storage definition
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/builder/main.go
+++ src/builder/main.go
@@ -17,6 +17,40 @@
 // Package builder provides all the solbuild specific functionality
 package builder
 
-// A Configuration contains all the data required to perform builds
-type Configuration struct {
+import (
+	"fmt"
+	"path/filepath"
+)
+
+const (
+	// ImagesDir is where we keep the rootfs images for build profiles
+	ImagesDir = "/var/lib/solbuild/images"
+
+	// ImageSuffix is the common suffix for all solbuild images
+	ImageSuffix = ".img"
+
+	// ImageCompressedSuffix is the common suffix for a fetched evobuild image
+	ImageCompressedSuffix = ".img.xz"
+
+	// ImageBaseURI is the storage area for base images
+	ImageBaseURI = "https://solus-project.com/image_root"
+)
+
+// A BackingImage is the core of any given profile
+type BackingImage struct {
+	Name        string // Name of the profile
+	ImagePath   string // Absolute path to the .img file
+	ImagePathXZ string // Absolute path to the .img.xz file
+	ImageURI    string // URI of the image origin
+}
+
+// NewBackingImage will return a correctly configured backing image for
+// usage.
+func NewBackingImage(name string) *BackingImage {
+	return &BackingImage{
+		Name:        name,
+		ImagePath:   filepath.Join(ImagesDir, name+ImageSuffix),
+		ImagePathXZ: filepath.Join(ImagesDir, name+ImageCompressedSuffix),
+		ImageURI:    fmt.Sprintf("%s/%s%s", ImageBaseURI, name, ImageCompressedSuffix),
+	}
 }
