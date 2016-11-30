11cc01bab92fdbb3a72c5e6d17829de5c57c791e
Ikey Doherty <ikey@solus-project.com>
2016-11-30 19:07:16 +0000

    update: Now actually mount the rootfs
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/builder/update.go
+++ src/builder/update.go
@@ -18,10 +18,48 @@ package builder
 
 import (
 	"errors"
+	log "github.com/Sirupsen/logrus"
+	"github.com/solus-project/libosdev/disk"
+	"os"
 )
 
 // Update will attempt to update the backing image to the latest version
 // internally.
 func (b *BackingImage) Update() error {
+	// TODO: Check if it is locked!
+
+	mountMan := disk.GetMountManager()
+	defer mountMan.UnmountAll()
+
+	log.WithFields(log.Fields{
+		"image": b.Name,
+	}).Info("Updating backing image")
+
+	if !PathExists(b.RootDir) {
+		if err := os.MkdirAll(b.RootDir, 00755); err != nil {
+			log.WithFields(log.Fields{
+				"error": err,
+			}).Error("Failed to create required directories")
+			return err
+		}
+		log.WithFields(log.Fields{
+			"dir": b.RootDir,
+		}).Debug("Created root directory")
+	}
+
+	log.WithFields(log.Fields{
+		"image": b.ImagePath,
+		"root":  b.RootDir,
+	}).Debug("Mounting rootfs")
+
+	// Mount the rootfs
+	if err := mountMan.Mount(b.ImagePath, b.RootDir, "auto", "loop"); err != nil {
+		log.WithFields(log.Fields{
+			"image": b.ImagePath,
+			"error": err,
+		}).Error("Failed to mount rootfs")
+		return err
+	}
+
 	return errors.New("Not yet implemented")
 }
