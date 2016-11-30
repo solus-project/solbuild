2130912a1eb29af70caa3cb0c4db7c68295fdbe7
Ikey Doherty <ikey@solus-project.com>
2016-11-30 23:30:33 +0000

    users: Add the (probably broken) parser
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/builder/users.go
+++ src/builder/users.go
@@ -17,29 +17,35 @@
 package builder
 
 import (
-	"errors"
+	"bufio"
+	"fmt"
+	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 // A User is an /etc/passwd defined user
 type User struct {
-	Name  string
-	UID   int
-	GID   int
-	Gecos string
-	Home  string
-	Shell string
+	Name  string // User Name
+	UID   int    // User ID
+	GID   int    // User primary Group ID
+	Gecos string // User Gecos (Pretty name)
+	Home  string // User home directory
+	Shell string // User shell program
 }
 
 // A Group is an /etc/group defined user
 type Group struct {
-	GID int
+	Name    string   // Group Name
+	ID      int      // Group ID
+	Members []string // Names of users in group
 }
 
 // Passwd is a simple helper to parse passwd files from a chroot
 type Passwd struct {
 	Users  map[string]*User
-	Groups map[int]*Group
+	Groups map[string]*Group
 }
 
 // NewPasswd will parse the given path and return a friendly representation
@@ -62,10 +68,79 @@ func NewPasswd(path string) (*Passwd, error) {
 
 // ParseUsers will attempt to parse a *NIX style passwd file
 func ParseUsers(passwd string) (map[string]*User, error) {
-	return nil, errors.New("Not yet implemented")
+	fi, err := os.Open(passwd)
+	if err != nil {
+		return nil, err
+	}
+	defer fi.Close()
+
+	ret := make(map[string]*User)
+
+	sc := bufio.NewScanner(fi)
+	for sc.Scan() {
+		line := sc.Text()
+		splits := strings.Split(line, ":")
+		if len(splits) != 7 {
+			return nil, fmt.Errorf("Invalid number of fields in passwd file: %d", len(splits))
+		}
+		user := &User{
+			Name:  strings.TrimSpace(splits[0]),
+			Gecos: strings.TrimSpace(splits[4]),
+			Home:  strings.TrimSpace(splits[5]),
+			Shell: strings.TrimSpace(splits[6]),
+		}
+		// Parse the uid/gid
+		if uid, err := strconv.Atoi(strings.TrimSpace(splits[2])); err == nil {
+			user.UID = uid
+		} else {
+			return nil, err
+		}
+		if gid, err := strconv.Atoi(strings.TrimSpace(splits[3])); err == nil {
+			user.GID = gid
+		} else {
+			return nil, err
+		}
+		// Success
+		ret[user.Name] = user
+	}
+	if err := sc.Err(); err != nil {
+		return nil, err
+	}
+	return ret, nil
 }
 
 // ParseGroups will attempt to parse a *NIX style group file
-func ParseGroups(grps string) (map[int]*Group, error) {
-	return nil, errors.New("Not yet implemented")
+func ParseGroups(grps string) (map[string]*Group, error) {
+	fi, err := os.Open(grps)
+	if err != nil {
+		return nil, err
+	}
+	defer fi.Close()
+
+	ret := make(map[string]*Group)
+
+	sc := bufio.NewScanner(fi)
+	for sc.Scan() {
+		line := sc.Text()
+		splits := strings.Split(line, ":")
+		if len(splits) != 4 {
+			return nil, fmt.Errorf("Invalid number of fields in group file: %d", len(splits))
+		}
+		group := &Group{
+			Name:    strings.TrimSpace(splits[0]),
+			Members: strings.Split(strings.TrimSpace(splits[3]), ","),
+		}
+		// Parse the gid
+		if gid, err := strconv.Atoi(strings.TrimSpace(splits[2])); err == nil {
+			group.ID = gid
+		} else {
+			return nil, err
+		}
+		// Success
+		ret[group.Name] = group
+	}
+	if err := sc.Err(); err != nil {
+		return nil, err
+	}
+	return ret, nil
 }
