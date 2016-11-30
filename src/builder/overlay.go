32ff22238d670a013747413103e58158d1d193ad
Ikey Doherty <ikey@solus-project.com>
2016-11-30 21:33:23 +0000

    builder: Add skeleton for the OverlayFS support
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/builder/overlay.go
@@ -0,0 +1,38 @@
+//
+// Copyright © 2016 Ikey Doherty <ikey@solus-project.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package builder
+
+import (
+	// We'll end up using this later
+	_ "github.com/Sirupsen/logrus"
+)
+
+// An Overlay is formed from a backing image & Package combination.
+// Using this Overlay we can bring up new temporary build roots using the
+// overlayfs kernel module.
+type Overlay struct {
+	Back    *BackingImage
+	Package *Package
+}
+
+// NewOverlay creates a new Overlay for us in builds, etc.
+func NewOverlay(back *BackingImage, pkg *Package) *Overlay {
+	return &Overlay{
+		Back:    back,
+		Package: pkg,
+	}
+}
