1f0d3de4164bb9e8eb188b5cb0a5ced22a404851
Ikey Doherty <ikey@solus-project.com>
2016-12-02 00:03:38 +0000

    builder: Start working on a Manager implementation
    At the moment the code is kind of all over the place and highly
    duplicated. We're also still unable to cleanly tear down our session
    when ctrl+c'd due to the pissed spaghetti nature of the codebase.
    
    We'll move towards a builder.Manager instance which can be responsible
    for keeping everything, well. Managed. :P
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/builder/manager.go
@@ -0,0 +1,41 @@
+//
+// Copyright © 2016 Ikey Doherty <ikey@solus-project.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package builder
+
+import (
+	"sync"
+)
+
+// A Manager is responsible for cleanly managing the entire session within solbuild,
+// i.e. setup, teardown, cleaning up, etc.
+//
+// The consumer should create a new manager instance and only use these methods,
+// not bypass and use API methods.
+type Manager struct {
+	overlay    *Overlay
+	pkg        *Package
+	pkgManager *EopkgManager
+
+	lock *sync.Mutex
+}
+
+// NewManager will return a newly initialised manager instance
+func NewManager() *Manager {
+	man := &Manager{}
+	man.lock = new(sync.Mutex)
+	return man
+}
