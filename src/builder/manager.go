18db640a874fbdb71b1fd54df9d04cde594a99fb
Ikey Doherty <ikey@solus-project.com>
2016-12-02 01:32:27 +0000

    manager: only kill, in case ypkg is being a dick
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/builder/manager.go
+++ src/builder/manager.go
@@ -156,7 +156,7 @@ func (m *Manager) Cleanup() {
 
 	// Try to kill the active root PID first
 	if m.activePID > 0 {
-		syscall.Kill(m.activePID, syscall.SIGTERM)
+		syscall.Kill(m.activePID, syscall.SIGKILL)
 		time.Sleep(2 * time.Second)
 		syscall.Kill(m.activePID, syscall.SIGKILL)
 		m.activePID = 0
