990ee83c67e90a875c46bc4fa2ed8edfe2881724
Ikey Doherty <ikey@solus-project.com>
2016-12-05 02:47:46 +0000

    Bump 1.0.3
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 PROJECT_ROOT := src/
-VERSION = 1.0.2
+VERSION = 1.0.3
 
 .DEFAULT_GOAL := all
 
--- src/solbuild/cmd/version.go
+++ src/solbuild/cmd/version.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	// SolbuildVersion is the current public version of solbuild
-	SolbuildVersion = "1.0.2"
+	SolbuildVersion = "1.0.3"
 )
 
 var versionCmd = &cobra.Command{
