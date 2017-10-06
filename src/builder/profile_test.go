ed1933b80bb59cba5557b47d67e8f10211a5c25b
Ikey Doherty <ikey@solus-project.com>
2017-10-06 21:37:34 +0100

    Helps if you pass your own tests, really.
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/builder/profile_test.go
+++ src/builder/profile_test.go
@@ -40,7 +40,7 @@ func TestLoadProfile(t *testing.T) {
 		t.Fatalf("Wrong image in profile: %v", profile.Image)
 	}
 	if repo, ok := profile.Repos["Solus"]; ok {
-		if repo.URI != "http://packages.solus-project.com/unstable/eopkg-index.xml.xz" {
+		if repo.URI != "https://packages.solus-project.com/unstable/eopkg-index.xml.xz" {
 			t.Fatalf("Wrong Solus URI: %v", repo.URI)
 		}
 	} else {
